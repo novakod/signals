3dd76c2abda20bc4e00d2a6f3cccc27514df2099
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2023-11-16 17:09:30 +0300

    Написаны базовые тесты для глубоких сигналов

---
+++ test/deep-signals.test.ts
@@ -0,0 +1,202 @@
+import { test, expect, vitest } from "vitest";
+import { createDeepSignal, createDeepEffect } from "../src/deep-signals";
+
+test("Тестирования глубоких сигналов на функции, в которой не используются сигналы", () => {
+  const signal = createDeepSignal({
+    count: 0,
+  });
+
+  const spy = vitest.fn();
+
+  createDeepEffect(spy);
+
+  expect(spy).toBeCalledTimes(1);
+
+  signal.count++;
+
+  expect(spy).toBeCalledTimes(1);
+});
+
+test("Тестирования глубоких сигналов на функции, в которой используются сигналы", () => {
+  const signal = createDeepSignal({
+    count: 0,
+  });
+
+  const spy = vitest.fn(() => {
+    signal.count;
+  });
+
+  createDeepEffect(spy);
+
+  expect(spy).toBeCalledTimes(1);
+
+  signal.count++;
+
+  expect(spy).toBeCalledTimes(2);
+});
+
+test("Тестирования глубоких сигналов на функции, в которой используется несколько сигналов", () => {
+  let dummy;
+  const signal1 = createDeepSignal({
+    count: 0,
+  });
+  const signal2 = createDeepSignal({
+    count: 0,
+  });
+
+  const spy = vitest.fn(() => {
+    dummy = signal1.count + signal2.count;
+  });
+
+  createDeepEffect(spy);
+
+  expect(spy).toBeCalledTimes(1);
+  expect(dummy).toBe(0);
+
+  signal1.count++;
+
+  expect(spy).toBeCalledTimes(2);
+  expect(dummy).toBe(1);
+
+  signal2.count++;
+
+  expect(spy).toBeCalledTimes(3);
+  expect(dummy).toBe(2);
+});
+
+test("Тестирование глубоких сигналов на нескольких эффектах", () => {
+  const signal = createDeepSignal({
+    count: 0,
+  });
+
+  const spy1 = vitest.fn(() => {
+    signal.count;
+  });
+  const spy2 = vitest.fn(() => {
+    signal.count;
+  });
+
+  createDeepEffect(spy1);
+  createDeepEffect(spy2);
+
+  expect(spy1).toBeCalledTimes(1);
+  expect(spy2).toBeCalledTimes(1);
+
+  signal.count++;
+
+  expect(spy1).toBeCalledTimes(2);
+  expect(spy2).toBeCalledTimes(2);
+});
+
+test("Тестирование глубоких сигналов, с использованием цепочки вызовов функций", () => {
+  let dummy;
+  const signal = createDeepSignal({
+    count: 0,
+  });
+
+  const getSignalValue = () => signal.count;
+
+  const spy = vitest.fn(() => {
+    dummy = getSignalValue();
+  });
+
+  createDeepEffect(spy);
+
+  expect(spy).toBeCalledTimes(1);
+  expect(dummy).toBe(0);
+
+  signal.count++;
+
+  expect(spy).toBeCalledTimes(2);
+  expect(dummy).toBe(1);
+
+  signal.count++;
+
+  expect(spy).toBeCalledTimes(3);
+  expect(dummy).toBe(2);
+});
+
+test("Тестирование обнаружения зависимостей, до которых нельзя дойти при первом запуске функции", () => {
+  const signal1 = createDeepSignal({
+    count: 0,
+  });
+  const signal2 = createDeepSignal({
+    count: 0,
+  });
+
+  const spy1 = vitest.fn();
+  const spy2 = vitest.fn();
+
+  createDeepEffect(() => {
+    if (signal1.count < 2) {
+      spy1();
+    } else if (signal2.count >= 0) {
+      spy2();
+    }
+  });
+
+  expect(spy1).toBeCalledTimes(1);
+  expect(spy2).toBeCalledTimes(0);
+
+  signal1.count = 2;
+
+  expect(spy1).toBeCalledTimes(1);
+  expect(spy2).toBeCalledTimes(1);
+
+  signal2.count = 5;
+
+  expect(spy1).toBeCalledTimes(1);
+  expect(spy2).toBeCalledTimes(2);
+});
+
+test("Тестирование эффекта, в котором изменение сигнала неактивной ветки не приводит к вызову эффекта", () => {
+  let dummy;
+  const signal1 = createDeepSignal({
+    field: {
+      value: true,
+    },
+  });
+  const signal2 = createDeepSignal({
+    field: {
+      value: "value",
+    },
+  });
+
+  const spy = vitest.fn(() => {
+    dummy = signal1.field.value ? signal2.field.value : "other";
+  });
+  createDeepEffect(spy);
+
+  expect(dummy).toBe("value");
+  expect(spy).toBeCalledTimes(1);
+
+  signal1.field.value = false;
+  expect(dummy).toBe("other");
+  expect(spy).toBeCalledTimes(2);
+});
+
+test("Тестирование глубокого эффекта, который не должен вызываться если в глубокий сигнал устанавливается такое же значение", () => {
+  let dummy;
+  const signal = createDeepSignal({
+    value: 0,
+  });
+
+  const spy = vitest.fn(() => {
+    dummy = signal.value;
+  });
+
+  createDeepEffect(spy);
+
+  expect(dummy).toBe(0);
+  expect(spy).toBeCalledTimes(1);
+
+  signal.value = 2;
+
+  expect(dummy).toBe(2);
+  expect(spy).toBeCalledTimes(2);
+
+  signal.value = 2;
+
+  expect(dummy).toBe(2);
+  expect(spy).toBeCalledTimes(2);
+});
