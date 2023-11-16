95af0631a75b493e082aac6d7c4e12079fcded87
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2023-11-16 12:29:31 +0300

    Добавлена первая версия глубоких сигналов

---
+++ src/deep-signals.ts
@@ -0,0 +1,82 @@
+import { createDeepProxy } from "@novakod/deep-proxy";
+
+let currentDeepEffect: DeepEffect | null = null;
+
+class DeepSignal<Value extends object> {
+  private subscribers: Map<string, Set<VoidFunction>> = new Map();
+  readonly proxifiedValue: Value;
+
+  constructor(value: Value) {
+    const signal = this;
+    this.proxifiedValue = createDeepProxy(value, {
+      get({ target, key, path, reciever }) {
+        if (currentDeepEffect) currentDeepEffect.addDependency(path.join("."), signal);
+        return Reflect.get(target, key, reciever);
+      },
+      set({ target, key, path, value, reciever }) {
+        if (Object.is(target[key], value)) return true;
+
+        const setResult = Reflect.set(target, key, value, reciever);
+
+        if (setResult) signal.runSubscribers(path.join("."));
+
+        return setResult;
+      },
+    });
+  }
+
+  runSubscribers(path: string) {
+    this.subscribers.get(path)?.forEach((subscriber) => subscriber());
+  }
+
+  subscribe(path: string, cb: VoidFunction) {
+    if (!this.subscribers.has(path)) {
+      this.subscribers.set(path, new Set());
+    }
+
+    this.subscribers.get(path)!.add(cb);
+  }
+
+  unsubscribe(path: string, cb: VoidFunction) {
+    this.subscribers.get(path)?.delete(cb);
+  }
+}
+
+export function createDeepSignal<Value extends object>(value: Value): Value {
+  const signal = new DeepSignal(value);
+
+  return signal.proxifiedValue;
+}
+
+class DeepEffect {
+  private deps: Map<string, Set<DeepSignal<any>>> = new Map();
+  private cb: VoidFunction;
+
+  constructor(cb: VoidFunction) {
+    this.cb = () => {
+      currentDeepEffect = this;
+      cb();
+      currentDeepEffect = null;
+    };
+    this.cb();
+  }
+
+  addDependency(path: string, signal: DeepSignal<any>) {
+    if (!this.deps.has(path)) {
+      this.deps.set(path, new Set());
+    }
+
+    this.deps.get(path)!.add(signal);
+    signal.subscribe(path, this.cb);
+  }
+
+  removeDependency(path: string, signal: DeepSignal<any>) {
+    this.deps.get(path)?.delete(signal);
+
+    signal.unsubscribe(path, this.cb);
+  }
+}
+
+export function createDeepEffect(cb: VoidFunction) {
+  return new DeepEffect(cb);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,38 @@
-import { createEffect, createSignal } from "./signals";
+import { createDeepEffect, createDeepSignal } from "./deep-signals";
 
-// debugger;
-const signal1 = createSignal(0);
-const signal2 = createSignal(0);
-
-createEffect(() => {
-  if (signal1.value < 2) {
-    console.log("1");
-  } else if (signal2.value >= 0) {
-    console.log("2");
-  }
+const signal = createDeepSignal({
+  count: 0,
+  testField: {
+    testField2: 0,
+    testField3: {
+      count: 0,
+      testArray: [1, 2, 3],
+    },
+  },
 });
 
-signal1.value = 2;
+createDeepEffect(() => {
+  console.log("effect1: ", signal.count);
+});
 
-signal2.value = 5;
+createDeepEffect(() => {
+  console.log("effect2: ", signal.testField.testField3.count);
+});
+
+const button = document.getElementById("button1");
+
+button?.addEventListener("click", () => {
+  signal.count = signal.count + 1;
+});
+
+const button2 = document.getElementById("button2");
+
+button2?.addEventListener("click", () => {
+  signal.testField.testField3.count++;
+});
+
+const button3 = document.getElementById("button3");
+
+button3?.addEventListener("click", () => {
+  signal.testField.testField3.testArray.push(signal.testField.testField3.testArray.length);
+});
