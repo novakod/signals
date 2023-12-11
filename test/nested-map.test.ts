daf599e0358919451346eb7785a214d3a55b9ab2
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2023-12-11 23:43:04 +0300

    Добавлены дополнительные тесты

--- test/nested-map.test.ts
+++ test/nested-map.test.ts
@@ -5,37 +5,43 @@ test.only("Тестирование класса NestedMap", () => {
   const nestedMap = new NestedMap();
 
   expect(nestedMap.set(["a", "b", "c"], "test_abc")).toBe(nestedMap);
-
   expect(nestedMap.set(["a", "b"], "test_ab")).toBe(nestedMap);
-
   expect(nestedMap.set(["a", "b", "c", "d"], "test_abcd")).toBe(nestedMap);
-
   expect(nestedMap.get(["a", "b", "c"])).toBe("test_abc");
-
   expect(nestedMap.get(["a", "b"])).toBe("test_ab");
-
   expect(nestedMap.get(["a", "b", "c", "d"])).toBe("test_abcd");
-
   expect(nestedMap.get(["a"])).toBe(undefined);
-
   expect(nestedMap.has(["a", "b"])).toBe(true);
-
   expect(nestedMap.has(["a"])).toBe(false);
 
   const spy = vitest.fn<[unknown, path: (string | number | symbol)[]]>();
 
   nestedMap.forEach(spy);
-
   expect(spy).toBeCalledTimes(3);
   expect(spy.mock.calls[0]).toEqual(["test_abc", ["a", "b", "c"]]);
   expect(spy.mock.calls[1]).toEqual(["test_ab", ["a", "b"]]);
   expect(spy.mock.calls[2]).toEqual(["test_abcd", ["a", "b", "c", "d"]]);
-
   expect(nestedMap.delete(["a", "b", "c"])).toBe(true);
-
   expect(nestedMap.delete(["a", "b", "c"])).toBe(false);
-
   expect(nestedMap.delete(["a", "b"])).toBe(true);
-
   expect(nestedMap.delete(["a"])).toBe(false);
+
+  const symbol = Symbol("test");
+
+  expect(nestedMap.set([symbol], "test")).toBe(nestedMap);
+  expect(nestedMap.set([Symbol.for("test")], "for_test")).toBe(nestedMap);
+  expect(nestedMap.get(["test", symbol])).toBe(undefined);
+  expect(nestedMap.get([Symbol("test")])).toBe(undefined);
+  expect(nestedMap.get([Symbol.for("test")])).toBe("for_test");
+  expect(nestedMap.delete([Symbol("test")])).toBe(false);
+  expect(nestedMap.delete([symbol])).toBe(true);
+  expect(nestedMap.delete([symbol])).toBe(false);
+  expect(nestedMap.delete([Symbol.for("test")])).toBe(true);
+  expect(nestedMap.delete([Symbol.for("test")])).toBe(false);
+
+  expect(nestedMap.set([0, 1], "numbers")).toBe(nestedMap);
+  expect(nestedMap.get([0, 1])).toBe("numbers");
+  expect(nestedMap.get([0])).toBe(undefined);
+  expect(nestedMap.delete([0, 1])).toBe(true);
+  expect(nestedMap.delete([0, 1])).toBe(false);
 });
