b1f3fa80a6062caf42e414a8f1baa31f42586958
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2024-02-03 00:54:45 +0300

    Новая версия пакета

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 export { createDeepSignal, DeepSignal, createDeepEffect, DeepEffect, type DeepEffectCb, type DeepEffectCbChange, deepBatch, deepCompute, deepUntrack } from "./deep-signals";
 export { createSignal, Signal, createEffect, Effect } from "./signals";
-export * from "@novakod/deep-proxy";
