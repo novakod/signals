9c8e9d1ff3b6d56dac529d52b27e7a1cd808da10
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2024-02-02 21:24:18 +0300

    Добавлены реэкспорты из пакета deep-proxy

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { createDeepSignal, DeepSignal, createDeepEffect, DeepEffect, type DeepEffectCb, type DeepEffectCbChange, deepBatch, deepCompute, deepUntrack } from "./deep-signals";
 export { createSignal, Signal, createEffect, Effect } from "./signals";
+export * from "@novakod/deep-proxy";
