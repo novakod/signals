f9804231c720347389c827701fa48e89476157c7
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2024-10-08 01:44:23 +0300

    Изменения в сборке

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "Signals",
-      fileName: "index",
+      fileName: "signals",
     },
   },
 });
